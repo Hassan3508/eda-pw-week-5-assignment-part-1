d2fcbe5fdc1a3f34248b6f1f1306538d09172931
ismail hassan <ismailshariff350@gmail.com>
2024-12-02 21:16:01 -0600

    object created with my first name property

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,6 +9,9 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
+  firstName: "ismail",
+  
+  
   // TODO - add properties here
 };
 console.log('A little about me:', me);
@@ -21,12 +24,10 @@ console.log('A little about me:', me);
 */
 
 
-
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
 
 
 /* 4. Change a property of an existing object.
@@ -35,7 +36,6 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
